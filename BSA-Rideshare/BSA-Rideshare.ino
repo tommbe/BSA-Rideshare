c5b73cc2119f759123dd5bf1ddc7b324b7d0b593
TomBe <tombevan26@me.com>
2025-05-13 10:10:21 +0100

    Changed Code to use functions

--- BSA-Rideshare/BSA-Rideshare.ino
+++ BSA-Rideshare/BSA-Rideshare.ino
@@ -1,69 +1,93 @@
-#include <SPI.h>                  // needed for SD Card
-#include <SD.h>                   // Library for SD card
-#include <Wire.h>                 // library for I2C
-#include "SparkFun_MMA8452Q.h"    // Accelerometer Library
-#include <Adafruit_BMP085.h>      // Library for BMP180 preassure and temperature sensor
-#include <Adafruit_HDC302x.h>     // Temp & Humidity Sensor Libary
+#include <SPI.h>       // needed for SD Card
+#include <SD.h>        // Library for SD card
+#include <Wire.h>        // library for I2C
+#include "SparkFun_MMA8452Q.h"       // Accelerometer Library
+#include <Adafruit_BMP085.h>         // Library for BMP180 pressure and temperature sensor
+#include <Adafruit_HDC302x.h>     // Temp & Humidity Sensor Library
 
-int PoleDelay = 1000;               // delay between sensor readings (mili sec) min =1
-const char* filename = ".csv";
+// Constants
+const int POLE_DELAY = 5000;       // delay between sensor readings (milliseconds) min = 1
+const char* FILENAME = "TEST.csv";
+const int CHIP_SELECT = 4;
+const int LED_PIN = LED_BUILTIN; // Use the built-in LED pin
 
-
-const int chipSelect = 4;
-
-MMA8452Q accel; 
+// Global Objects
+MMA8452Q accel;
 Adafruit_BMP085 bmp;
 Adafruit_HDC302x hdc = Adafruit_HDC302x();
+File dataFile; //make the file global
 
+// Function Declarations (Best Practice)
+void setupSDCard();
+void setupAccelerometer();
+void setupBMP180();
+void setupHDC302x();
+void createDataFile();
+void readAndLogData();
+void printSensorData(float accX, float accY, float accZ, float tempA, float pres, double tempB, double RH); // Used for debuging and testing
 void setup() {
   Serial.begin(9600);
-  Serial.println("MMA8452Q Basic Reading Code!");
   Wire.begin();
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 
-  // Initialise SD card
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    while (1);                      // Stops code if 
-  }
+  setupSDCard();
+  setupAccelerometer();
+  setupBMP180();
+  setupHDC302x();
+  createDataFile();
+}
 
-  // Accelerotmeter Setup
-  if (accel.begin() == false) {
-    Serial.println("Not Connected. Please check connections and read the hookup guide.");
+void loop() {
+  delay(POLE_DELAY);
+  readAndLogData();
+}
+
+void setupSDCard() {
+  if (!SD.begin(CHIP_SELECT)) {
+    Serial.println("SD Card failed, or not present");
     while (1);
   }
+}
 
-  // Preassure and Temp Setup
+void setupAccelerometer() {
+  if (accel.begin() == false) {
+    Serial.println("Could not find MMA8452Q (accelerometer) sensor");
+    while (1);
+  }
+}
+
+void setupBMP180() {
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
+    Serial.println("Could not find BMP180 (Preassure Sensor) sensor");
+    while (1) {}
   }
+}
 
-  // Humidity and Temp Sensor
-  if (! hdc.begin(0x44, &Wire)) {
-  Serial.println("Could not find sensor?");
-  while (1);
+void setupHDC302x() {
+  if (!hdc.begin(0x44, &Wire)) {
+    Serial.println("Could not find HDC302x (Temp & Humidity) sensor");
+    while (1);
   }
+}
 
-  // Create a new file
-  File dataFile = SD.open(filename, FILE_WRITE);
+void createDataFile() {
+  dataFile = SD.open(FILENAME, FILE_WRITE);
 
   if (dataFile) {
-    dataFile.println("AccX,AccY,AccZ,TempBMP180,Presure,TempHDC302x,Humidity");
+    dataFile.println("AccX,AccY,AccZ,TempBMP180,Pressure,TempHDC302x,Humidity");
     dataFile.close();
     Serial.print("Created file: ");
-    Serial.println(filename);
-  } 
+    Serial.println(FILENAME);
+  }
   else {
     Serial.println("Error opening data file for writing.");
   }
 }
 
-void loop() {
-  delay(PoleDelay);
 
-  digitalWrite(LED_BUILTIN, LOW);
+void readAndLogData() {
+  digitalWrite(LED_PIN, LOW); // Removed:  Use meaningful names
 
   float accX = accel.getCalculatedX();
   float accY = accel.getCalculatedY();
@@ -76,8 +100,7 @@ void loop() {
   double RH = 0.0;
   hdc.readTemperatureHumidityOnDemand(tempB, RH, TRIGGERMODE_LP0);
 
-   // Open the file for appending
-  File dataFile = SD.open(filename, FILE_WRITE);
+  dataFile = SD.open(FILENAME, FILE_WRITE);
 
   // Write the sensor data to the file
   if (dataFile) {
@@ -99,10 +122,16 @@ void loop() {
 
     dataFile.close();
     Serial.println("Data logged to SD card.");
-  
-  digitalWrite(LED_BUILTIN, HIGH);
 
-  // Testing Print Outs
+    digitalWrite(LED_PIN, HIGH);
+    printSensorData(accX, accY, accZ, tempA, pres, tempB, RH); //call print
+  } else {
+    Serial.println("Error opening data file for appending.");
+  }
+}
+
+//function to print the sensor data
+void printSensorData(float accX, float accY, float accZ, float tempA, float pres, double tempB, double RH) {
   Serial.println("Acceleration");
   Serial.print("AccX: ");
   Serial.println(accX, 3);
@@ -110,8 +139,8 @@ void loop() {
   Serial.println(accY, 3);
   Serial.print("AccZ: ");
   Serial.println(accZ, 3);
-  Serial.println("Preassure & Temp");
-  Serial.print("Preassure: ");
+  Serial.println("Pressure & Temp");
+  Serial.print("Pressure: ");
   Serial.println(pres);
   Serial.print("Temperature A: ");
   Serial.println(tempA);
@@ -120,12 +149,5 @@ void loop() {
   Serial.println(tempB);
   Serial.print("Humidity: ");
   Serial.println(RH);
-
-
   Serial.println("");
-
-  } else {
-    Serial.println("Error opening data file for appending.");
-  }
-
 }
